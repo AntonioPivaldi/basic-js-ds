47de6e0f9b50b1f6585f07b203c15ace968a08df
AntonioPivaldi <9212565@gmail.com>
2022-05-03 02:03:29 +0300

    init: start js-ds

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  stackYeah = [];
+
+  push(element) {
+    return this.stackYeah.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackYeah.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let length = this.stackYeah.length;
+    return this.stackYeah[length - 1];
   }
 }
 
